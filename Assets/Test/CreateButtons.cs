093aa1450291e8ae0a32a6919880ad4c9c671fac
Paul Sinnett <paul.sinnett@gmail.com>
2018-07-27 19:03:27 +0100

    auto test problem

---
+++ Assets/Test/CreateButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateButtons : MonoBehaviour
+{
+    public GameObject buttonPrefab;
+    public int numberOfButtons = 3;
+
+    void Awake()
+    {
+        for (int i = 0; i < numberOfButtons; ++i)
+        {
+            Instantiate(buttonPrefab, transform);
+        }
+    }
+}
---
+++ Assets/Test/FunctionalTests.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class FunctionalTests
+{
+	[UnityTest]
+	public IEnumerator TestButtonHasInputFocus()
+	{
+		yield return SceneManager.LoadSceneAsync("TestMenu");
+        yield return null;
+        Assert.IsNotNull(EventSystem.current.currentSelectedGameObject, "no button selected");
+        yield return SceneManager.UnloadSceneAsync("TestMenu");
+	}
+
+	[UnityTest]
+	public IEnumerator TestButtonDoesNotLoseInputFocus()
+	{
+		yield return SceneManager.LoadSceneAsync("TestMenu");
+        yield return null;
+        yield return null;
+        GameObject selected = EventSystem.current.currentSelectedGameObject;
+        Assert.IsNotNull(selected, "no button selected");
+        CanvasGroup canvasGroup = selected.GetComponentInParent<CanvasGroup>();
+        Assert.IsNotNull(canvasGroup, "button is not controlled by a CanvasGroup");
+        Assert.IsTrue(canvasGroup.interactable, "CanvasGroup is not interactable");
+        AxisEventData axisEvent = new AxisEventData(EventSystem.current);
+        axisEvent.moveDir = MoveDirection.Up;
+        ExecuteEvents.Execute(EventSystem.current.currentSelectedGameObject, axisEvent, ExecuteEvents.moveHandler);
+        selected = EventSystem.current.currentSelectedGameObject;
+        Assert.AreEqual(selected.GetComponentInParent<CanvasGroup>(), canvasGroup, "the CanvasGroup changed after navigation");
+        yield return SceneManager.UnloadSceneAsync("TestMenu");
+	}
+}
